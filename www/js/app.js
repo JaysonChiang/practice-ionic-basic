5b6ff7728e3881bcfbe0f8208b32f335a81a90d8
Jayson <jayson@mac.local>
2018-02-12 23:25:02 +0800

    init commit

--- www/js/app.js
+++ www/js/app.js
@@ -3,8 +3,25 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
-
+angular.module('App', ['ionic'])
+.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+  .state('home',{
+    url:'/home',
+    templateUrl:'views/home/home.html'
+  })
+  .state('reservation',{
+    url:'/reservation',
+    controller:'ReservationController',
+    templateUrl:'views/reservation/reservation.html'
+  })
+  .state('weather',{
+    url:'/weather',
+    controller:'WeatherController',
+    templateUrl:'views/weather/weather.html'
+  });
+  $urlRouterProvider.otherwise('/home');
+})
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -16,6 +33,9 @@ angular.module('starter', ['ionic'])
       // from snapping when text inputs are focused. Ionic handles this internally for
       // a much nicer keyboard experience.
       cordova.plugins.Keyboard.disableScroll(true);
+
+
+
     }
     if(window.StatusBar) {
       StatusBar.styleDefault();
